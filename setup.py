bbc3b218bee8dc862923b67bbff52a8405a2ed15
gradedSystem <homesiknessgerald@gmail.com>
2025-04-13 08:23:13 +0800

    [add][m] publish description add

--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("version.txt", "r", encoding="utf-8") as fh:
+    version = fh.read().strip()
+
 setup(
     name="universal-clipboard",
-    version="1.0.0",
-    description="Cross-platform clipboard synchronization between Windows and Mac",
+    version=version,
     author="Your Name",
     author_email="your.email@example.com",
+    description="Cross-platform clipboard synchronization between Windows and Mac",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/yourusername/universal-clipboard",
     packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8",
     install_requires=[
         "websockets>=11.0.3",
         "cryptography>=41.0.3",
@@ -24,16 +44,9 @@ setup(
             "flake8>=6.0.0",
         ],
     },
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: End Users/Desktop",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: MacOS :: MacOS X",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
-    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [
+            "universal-clipboard=universal_clipboard.cli:main",
+        ],
+    },
 ) 
